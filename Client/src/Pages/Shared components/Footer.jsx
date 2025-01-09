d9f9c6ef1859eb9aba1b3e9ae46e70777809c6f4
AyeshaFerdous <ayeshaferdous01@gmail.com>
2025-01-10 02:14:02 +0600

    change some footer design

--- Client/src/Pages/Shared components/Footer.jsx
+++ Client/src/Pages/Shared components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
 import Logo from '../../assets/logo.png'
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
@@ -23,23 +23,38 @@ const Footer = () => {
           {/* Divider */}
           <hr className="border-white-700 my-6" />
           {/* Footer Content */}
-          <div className="flex flex-wrap justify-between text-center md:text-left">
+          <div className="grid grid-cols-1 md:grid-cols-3 text-center md:text-left">
             {/* Contact Info */}
-            <div className="mb-6 md:w-1/3">
+            <div className="mb-6 ">
               <h3 className="text-lg font-semibold text-white mb-2">
                 Contact Us
               </h3>
-              <p>
-                Email:{" "}
+              <p className="flex gap-3 mb-2">
+                <FaEnvelope className="text-2xl"></FaEnvelope>
                 <a
-                  href="mailto:shelfmatesupport@yourwebsite.com"
+                  href="mailto:athleonsupport@yourwebsite.com"
                   className="text-[#2375cc] hover:underline"
                 >
-                 shelfmatesupport@yourwebsite.com
+                  ayeshaferdous01@gmail.com
                 </a>
               </p>
-              <p>Phone: +1-234-567-890</p>
-              <p>Address: 12/D Banasree, Dhaka, Bangladesh</p>
+              <p className="flex gap-3 mb-2"><FaPhoneAlt className="text-2xl"></FaPhoneAlt><span>+8801305095882</span> </p>
+              <p className="flex gap-3 mb-2"><FaMapMarkerAlt className="text-2xl"></FaMapMarkerAlt><span>Maona, Gazipur, Bangladesh</span> </p>
+            </div>
+
+              {/* Services */}
+              <div className="mb-6">
+              <h3 className="text-lg font-semibold text-white mb-2">
+                Our Services
+              </h3>
+              <ul className="text-md">
+                <li><Link to="/queries" className="hover:text-white">Queries</Link></li>
+                <li><Link to="/my-queries" className="hover:text-white">My Queries</Link></li>
+                <li><Link to="/add-queries" className="hover:text-white">Add Queries</Link></li>
+                <li><Link to="/recommendations-for-me" className="hover:text-white"></Link>Recommendations For me</li>
+                <li><Link to="/my-recommendations" className="hover:text-white"></Link>My Recommendations</li>
+                <li><Link to="/home/contact" className="hover:text-white">Contact</Link></li>
+              </ul>
             </div>
             {/* Social Media Links */}
             <div className="mb-6 md:w-1/3">
@@ -48,43 +63,41 @@ const Footer = () => {
               </h3>
               <div className="flex justify-center md:justify-start space-x-4">
                 <a
-                  href="#"
-                  className="text-gray-300 hover:text-[#23cc8b]"
+                  href="https://www.facebook.com/ayesha.ferdous.94"
+                  className="text-gray-300 hover:text-white"
                   aria-label="Facebook"
                 >
                   <FaFacebook size={24} />
                 </a>
+               
                 <a
-                  href="#"
-                  className="text-gray-300 hover:text-[#23cc8b]"
-                  aria-label="Twitter"
-                >
-                  <FaTwitter size={24} />
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:text-[#23cc8b]"
+                  href="https://www.instagram.com/ayeshaferdous31/"
+                  className="text-gray-300 hover:text-white"
                   aria-label="Instagram"
                 >
                   <FaInstagram size={24} />
                 </a>
                 <a
-                  href="#"
-                  className="text-gray-300 hover:text-[#23cc8b]"
+                  href="https://www.linkedin.com/in/ayesha-ferdous-682419317/"
+                  className="text-gray-300 hover:text-white"
                   aria-label="LinkedIn"
                 >
                   <FaLinkedin size={24} />
                 </a>
               </div>
             </div>
-            {/* Copyright */}
-            <div className="md:w-1/3">
+         
+          </div>
+
+             {/* Divider */}
+         <hr className="border-white-700 my-6" />
+          {/* Copyright */}
+          <div className="text-center py-2">
               <p className="text-sm">
-                &copy; 2024 SHELFMATE. All rights reserved.
+                &copy; 2024 ATHLEON. All rights reserved.
               </p>
               <p>Designed with ❤️ by Ayesha's Team.</p>
             </div>
-          </div>
         </div>
       </footer>
     </div>
