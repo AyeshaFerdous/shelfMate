397194f88f3a1b9b3d88bed0cf538433274a92cd
AyeshaFerdous <ayeshaferdous01@gmail.com>
2025-02-11 01:19:57 +0600

    Install email.js

--- Client/src/Pages/Nested components/ContactSection.jsx
+++ Client/src/Pages/Nested components/ContactSection.jsx
@@ -1,6 +1,28 @@
+import { useRef } from "react";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const ContactSection = () => {
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_n2jsky2", 
+        "template_au6su7w", 
+        formRef.current,
+        "696y_P1JCqbyXfCHA" 
+      )
+      .then(
+        (result) => {
+          toast("Message sent successfully!");
+        },
+        (error) => {
+          toast.error("Failed to send message. Please try again.");
+        }
+      );
+  };
   return (
     <div className="bg-gradient-to-br from-[#b6dbf0] to-[#5bb4e6] text-white py-28">
       <div className="max-w-6xl mx-auto px-6 lg:px-8">
@@ -40,21 +62,24 @@ const ContactSection = () => {
         {/* Contact Form */}
         <div className="mt-16 bg-white text-black rounded-lg shadow-lg p-8 max-w-4xl mx-auto">
           <h3 className="text-2xl font-semibold text-center mb-6">Send Us a Message</h3>
-          <form>
+          <form ref={formRef} onSubmit={sendEmail}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
               <input
                 type="text"
+                name="name"
                 placeholder="Your Name"
                 className="w-full p-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5bb4e6]"
               />
               <input
                 type="email"
+                name="email"
                 placeholder="Your Email"
                 className="w-full p-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5bb4e6]"
               />
             </div>
             <textarea
               rows="5"
+              name="message"
               placeholder="Your Message"
               className="w-full p-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5bb4e6] mb-4"
             ></textarea>
